98694b06618faf7c9287733d372fbd5620053cae
Shawn Smith <shawnpsmith@gmail.com>
2014-02-22 23:49:43 +0900

    add stroke count tests

--- kanjidic2_test.go
+++ kanjidic2_test.go
@@ -5,6 +5,7 @@ import (
 )
 
 var numKanji = 13108
+var kanjiList []Kanji
 
 func TestParser(t *testing.T) {
 	got, err := ParseKanjiDic2("kanjidic2.xml")
@@ -13,5 +14,32 @@ func TestParser(t *testing.T) {
 	}
 	if len(got) != numKanji {
 		t.Fatalf("ParseKanjiDic2 length incorrect: got %d, want %d", got, numKanji)
+	}
+	// set kanjiList for later tests
+	kanjiList = got
+}
+
+var strokeCountTests = []struct {
+	Literal string
+	Count   int
+}{
+	{"ー", 1},
+	{"二", 2},
+	{"三", 3},
+	{"中", 4},
+	{"店", 8},
+	{"感", 13},
+	{"機", 16},
+}
+
+func TestStrokeCount(t *testing.T) {
+	for _, k := range kanjiList {
+		for _, tt := range strokeCountTests {
+			if k.Literal == tt.Literal {
+				if got := k.StrokeCount; got != tt.Count {
+					t.Errorf("TestStrokeCount (%s): got %d, want %d", tt.Literal, got, tt.Count)
+				}
+			}
+		}
 	}
 }
