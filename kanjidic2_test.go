16fc66ab3295b006c52a5d2137094ab13e09ee3e
Shawn Smith <shawnpsmith@gmail.com>
2014-02-22 17:41:46 +0900

    start on tests

---
+++ kanjidic2_test.go
@@ -0,0 +1,17 @@
+package kanjidic2
+
+import (
+	"testing"
+)
+
+var numKanji = 13108
+
+func TestParser(t *testing.T) {
+	got, err := ParseKanjiDic2("/Users/shawn/Downloads/kanjidic2.xml")
+	if err != nil {
+		t.Fatalf("ParseKanjiDic2: %v", err)
+	}
+	if len(got) != numKanji {
+		t.Fatalf("ParseKanjiDic2 length incorrect: got %d, want %d", got, numKanji)
+	}
+}
---
+++ setup.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+wget http://www.csse.monash.edu.au/\~jwb/kanjidic2/kanjidic2.xml.gz
+gunzip kanjidic2.xml.gz
